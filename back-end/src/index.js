f129ff5d57c73eae8c28d4f65cb8763cfe3127a7
GGrop <giuliano.gropponi@gmail.com>
2023-04-12 18:49:21 -0300

    add GET /teams

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,3 +3,29 @@ const express = require('express');
 const app = express();
 
 const PORT = '8007';
+<<<<<<< Updated upstream
+=======
+
+const fs = require('fs');
+
+function getTeams() {
+  const teams = JSON.parse(fs.readFileSync('./data/teams.db.json'));
+  const teamsLength = teams.length;
+  const dataTeams = {
+    teams,
+    length: teamsLength,
+  };
+  return dataTeams;
+}
+
+app.get('/teams', (req, res) => {
+  const dataTeams = getTeams();
+  res.status(200).json({
+    dataTeams,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log('listening on port', PORT);
+});
+>>>>>>> Stashed changes
