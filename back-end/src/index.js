6245994966fc8eb4b63d11c00b0cc079cdefbb08
GGrop <giuliano.gropponi@gmail.com>
2023-04-12 21:28:25 -0300

    add POST /new-team

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -6,6 +6,8 @@ const PORT = '8007';
 
 const fs = require('fs');
 
+app.use(express.json());
+
 function getTeams() {
   const teams = JSON.parse(fs.readFileSync('./data/teams.db.json'));
   const teamsLength = teams.length;
@@ -23,6 +25,44 @@ app.get('/teams', (req, res) => {
   });
 });
 
+function createNewTeam(name, tla, country, adress, website, founded) {
+  const dataTeams = getTeams();
+  const isDuplicated = dataTeams.teams.find((team) => team.tla === tla.toUpperCase());
+  let newTeam = {};
+  if (isDuplicated) {
+    return newTeam;
+  }
+  newTeam = {
+    name,
+    tla,
+    area: {
+      name: country,
+    },
+    adress,
+    website,
+    founded,
+  };
+  return newTeam;
+}
+
+app.post('/new-team', (req, res) => {
+  const dataTeams = getTeams();
+  const {
+    name, tla, country, adress, website, founded,
+  } = req.body;
+  const newTeam = createNewTeam(name, tla, country, adress, website, founded, getTeams());
+  if (!newTeam) {
+    console.log('mostrar error');
+  } else {
+    dataTeams.teams.push(newTeam);
+    fs.writeFile('./data/teams.db.json', JSON.stringify(dataTeams.teams), (err) => {
+      res.status(200).json({
+        dataTeams: getTeams(),
+      });
+    });
+  }
+});
+//bucle infinito en testing
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
