d6bec6ae9e3834097bf3e17f5c4aad48f9a3443e
GGrop <giuliano.gropponi@gmail.com>
2023-04-19 09:21:48 -0300

    add imports to crud.js

---
+++ front-end/src/crud.js
@@ -0,0 +1,11 @@
+import { getATeam, getTeamsList } from "./services/teamsServices.js";
+import { handleLoading } from "./ui/loading/loading.js";
+import showTeamList from "./ui/teamsLists/teamLists.js"
+import showTeamCard from './ui/teamCard/teamCard.js'
+// export async function  initialization () {
+//   const data = await getATeam("ARS")
+//   console.log(data)
+//   const data2= await getTeamsList()
+//   console.log(data2)
+// }
+
