b89ce6864e2b4daa27e944818948a1cd824bf433
GGrop <giuliano.gropponi@gmail.com>
2023-04-19 09:26:27 -0300

    add imports to teamServices.js

---
+++ front-end/src/services/teamsServices.js
@@ -0,0 +1,4 @@
+import { getATeam as getATeamAPI , getTeamList as getTeamListAPI } from "../api/teamsAPI.js";
+import { mapTeam, mapTeamsList } from "../entities_maps/maps.js";
+import { getATeam as getATeamStorage , getTeamsList as getTeamsListStorage, saveOnStorage } from "../storage/teamsStorage.js";
+
