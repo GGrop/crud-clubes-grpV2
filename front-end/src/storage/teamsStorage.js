8e0e40bed5d43d0883649e143a685737b5f4cd34
GGrop <giuliano.gropponi@gmail.com>
2023-04-19 09:29:39 -0300

    add GetTeamsList() to teamsStorage.js

--- front-end/src/storage/teamsStorage.js
+++ front-end/src/storage/teamsStorage.js
@@ -6,3 +6,11 @@ export function saveOnStorage(data,key){
   localStorage.setItem(makeKey(key), JSON.stringify(data))
 }
 
+export function getTeamsList(){
+  const teamsList = JSON.parse(localStorage.getItem(makeKey("List")))
+  if (teamsList === null){
+    throw new Error("there are no teams in the storage yet")
+  }
+  return teamsList
+}
+
