8d873ff64bed224f4875c27d5a2b90361710f4f3
GGrop <giuliano.gropponi@gmail.com>
2023-04-19 09:20:55 -0300

    rename API file

--- front-end/src/api/teamsAPI.js
+++ front-end/src/api/teamsAPI.js
@@ -3,10 +3,10 @@ const URL2 = 'http://localhost:8007/team';
 
 
 
-export async function getClubList() {
+export async function getTeamList() {
   return (await fetch(URL)).json()
 }
 
-export async function getAClub(TLA) {
+export async function getATeam(TLA) {
   return (await fetch(`${URL2}/${TLA}`)).json();
 }
