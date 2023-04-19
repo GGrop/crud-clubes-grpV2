2d812c03950f392e05e05b655b31b7509c5eaa6a
GGrop <giuliano.gropponi@gmail.com>
2023-04-19 09:25:29 -0300

    add mapTeam to map.js

--- front-end/src/entities_maps/maps.js
+++ front-end/src/entities_maps/maps.js
@@ -1,3 +1,26 @@
 import Team from '../entities/team.js'
 import TeamList from '../entities/teamList.js'
 
+export function mapTeam(teamData){
+  const {
+    name,
+    tla,
+    area,
+    crestUrl,
+    address,
+    website,
+    founded
+  } = teamData.myTeam
+  // console.log(country)
+
+  return new Team(
+    name,
+    tla,
+    area.name,
+    crestUrl,
+    address,
+    website,
+    founded
+  )
+}
+
