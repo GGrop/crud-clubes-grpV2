35d09ed9f3d01797f43fa738385f232db0a747e3
GGrop <giuliano.gropponi@gmail.com>
2023-04-21 13:11:16 -0300

    update routes from teamsAPI.js

--- front-end/src/api/teamsAPI.js
+++ front-end/src/api/teamsAPI.js
@@ -1,17 +1,22 @@
-const URL = 'http://localhost:8007/teams';
-const URL2 = 'http://localhost:8007/team';
+const URL = 'http://localhost:8007';
 
 export async function getTeamList() {
-  return (await fetch(URL)).json();
+  return (await fetch(`${URL}/teams`)).json();
 }
 
 export async function getATeam(tla) {
-  return (await fetch(`${URL2}/${tla}`)).json();
+  return (await fetch(`${URL}/team/${tla}`)).json();
 }
 
 export async function editTeam(tla, dataForm) {
-  await fetch(`http://localhost:8007/team/${tla}/edit`, {
+  await fetch(`${URL}/team/${tla}/edit`, {
     method: 'PUT',
     body: dataForm,
+  });
+}
+
+export async function resetTeams() {
+  await fetch(`${URL}/reset-teams`, {
+    method: 'PUT',
   });
 }
