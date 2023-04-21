086ed57753745bfdab002df632425025ebfd6f62
GGrop <giuliano.gropponi@gmail.com>
2023-04-21 13:13:12 -0300

    update imports from crud.js

--- front-end/src/crud.js
+++ front-end/src/crud.js
@@ -3,14 +3,14 @@ import { handleLoading } from './ui/loading/loading.js';
 import showTeamList from './ui/teamsLists/teamLists.js';
 import createTeamCard from './ui/teamCard/teamCard.js';
 import createEditableTeamCard from './ui/teamEdit/teamEdit.js';
-import { editTeam as editTeamApi } from './api/teamsAPI.js';
-import { showAlert } from './ui/alerts/alerts.js';
 // export async function  initialization () {
 //   const data = await getATeam("ARS")
 //   console.log(data)
 //   const data2= await getTeamsList()
 //   console.log(data2)
 // }
+import { editTeam as editTeamApi, resetTeams } from './api/teamsAPI.js';
+import { handleAlert } from './ui/alerts/alerts.js';
 
 function removeContent() {
   document.querySelector('#content').innerHTML = '';
